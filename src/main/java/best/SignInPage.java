3176e248800cd5af5b050eaa3828023e633d1863
Jinnoka3 <galinaprokofevna@gmail.com>
2019-07-17 04:28:25 +0300

    update with new tests (not ready)

--- src/main/java/best/SignInPage.java
+++ src/main/java/best/SignInPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.Date;
+
 public class SignInPage extends BasePage{
 
     private WebDriver driver;
@@ -13,7 +15,7 @@ public class SignInPage extends BasePage{
         super(driver);
     }
 
-    @FindBy(xpath = "//*[@id=\"email_create\"]")
+    @FindBy(xpath = "//input[@id='email_create']")
     private WebElement emailFirstPage;
 
     @FindBy(xpath = "//*[@id=\"SubmitCreate\"]/span")
@@ -25,9 +27,30 @@ public class SignInPage extends BasePage{
     @FindBy(xpath = "//*[@id=\"columns\"]/div[1]/span[2]")
     private WebElement titleCreateAnAccount;
 
+    @FindBy(xpath = "//input[@id='email']")
+    private WebElement emailRegistered;
+
+    @FindBy(xpath = "//input[@id='email']")
+    private WebElement passwordRegistered;
+
+   /* @FindBy(xpath = "//p[@class='submit']//span[1]")
+    private WebElement signIn;*/
+
     public void sendEmailFirstPage(String emailFirst) {
         emailFirstPage.sendKeys(emailFirst);
     }
+
+    public void sendEmail(String email) {
+        emailRegistered.sendKeys(email);
+    }
+
+    public void sendPassword(String password) {
+        passwordRegistered.sendKeys(password);
+    }
+
+    /*public void clickSignIn(){
+        signIn.click();
+    }*/
 
     public void clickCreateAnAccount(){
         createAnAccount.click();
@@ -39,10 +62,22 @@ public class SignInPage extends BasePage{
 
     public void signIn(AccountData accountData){
 
+        /*Date dat = new Date();
+        long now = dat.getTime();*/
+
         clickSignIn();
         sendEmailFirstPage(accountData.getEmail());
         clickCreateAnAccount();
         getTitle();
+    }
+
+    public void alreadyRegistered(AccountData accountData){
+
+        clickSignIn();
+        sendEmail(accountData.getEmail());
+        sendPassword(accountData.getEmail());
+        clickSignIn();
+        //getTitle();
     }
 
     @Override
